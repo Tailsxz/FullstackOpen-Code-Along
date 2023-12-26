45fb68f7c6039ad14a76fdb8dafef63b70a34d44
Tai Phan <ptai7722@gmail.com>
2023-12-26 14:27:14 -0600

    Set up a basic server, serving the plain text of "Hello World"

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello World!');
+})
+
+const PORT = process.env.PORT || 2277;
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
+
